5faa44bae9858f0bc1edd2933925f8d894c04e02
Iamneo <techadmin@iamneo.ai>
2023-08-02 06:29:22 +0000

    Backup Commit

---
+++ springapp/src/main/java/com/examly/springapp/ModelTask.java
@@ -0,0 +1,46 @@
+package com.examly.springapp;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class ModelTask {
+    @Id
+    int taskId;
+    String taskHolderName;
+    String taskDate;
+    String taskName;
+    public int getTaskId() {
+        return taskId;
+    }
+    public void setTaskId(int taskId) {
+        this.taskId = taskId;
+    }
+    public String getTaskHolderName() {
+        return taskHolderName;
+    }
+    public void setTaskHolderName(String taskHolderName) {
+        this.taskHolderName = taskHolderName;
+    }
+    public String getTaskDate() {
+        return taskDate;
+    }
+    public void setTaskDate(String taskDate) {
+        this.taskDate = taskDate;
+    }
+    public String getTaskName() {
+        return taskName;
+    }
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+    public String getTaskStatus() {
+        return taskStatus;
+    }
+    public void setTaskStatus(String taskStatus) {
+        this.taskStatus = taskStatus;
+    }
+    String taskStatus;
+   
+
+    
+}
